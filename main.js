85fa2117cae734998fb83632b8f213868e2e3c2b
gausszhou <gausszhou@qq.com>
2023-04-30 00:19:59 +0800

    Access Node.js from the renderer with a preload script

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+const path = require('path');
 const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadFile('index.html')
@@ -11,4 +15,13 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow()
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// quits an application entirely on exiting all windows
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
 })
