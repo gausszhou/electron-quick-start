39093114094bc9def715201c4706ca28c4f10732
gausszhou <gausszhou@qq.com>
2023-05-03 23:50:40 +0800

    Augmenting the renderer with a preload script

--- renderer.js
+++ renderer.js
@@ -1,1 +1,3 @@
 console.log('[info] hello this is renderer process')
+const information = document.getElementById('info')
+information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
